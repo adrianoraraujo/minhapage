a18a66d64021dce09c3340cf589f91a5d0644994
adrianoraraujo <31390563+adrianoraraujo@users.noreply.github.com>
2021-12-20 15:15:36 -0300

    Update index.php
    add C#

--- web/index.php
+++ web/index.php
@@ -169,6 +169,10 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
           <div class="w3-light-grey w3-round-xlarge w3-small">
             <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:90%">90%</div>
           </div>
+	  <p>C#</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:70%">70%</div>
+          </div>
            <p>CAD/CAN/CAE</p>
           <div class="w3-light-grey w3-round-xlarge w3-small">
             <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:60%">60%</div>
